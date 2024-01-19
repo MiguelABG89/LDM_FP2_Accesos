73d722a45ada11666bb94e42c4469626eaa5ac6a
Alumno_FP <200066@iessanalberto.com>
2024-01-19 08:36:14 +0100

    Creación menu

---
+++ src/main/java/Menu.java
@@ -0,0 +1,26 @@
+public class Menu {
+    public static void main(String[] args) {
+        boolean salir = false;
+        String opcion;
+        while (!salir) {
+            System.out.println("""
+                    *******************************************
+                    0. Salir
+                    1. Insertar proyecto
+                    2. Eliminar proyecto
+                    3. Modificar proyecto
+                    4. Mostrar proyectos
+                    *******************************************""");
+            opcion = libs.Leer.pedirCadena("Introduce una opción");
+
+            switch (opcion) {
+                case "0" -> salir = true;
+                case "1" -> {}
+                case "2" -> {}
+                case "3" -> {}
+                case "4" -> {}
+                default -> System.err.println("La opción introducida no es valida");
+            }
+        }
+    }
+}
