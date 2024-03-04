df4dd247ec524be64c519ac64b9c88e8f2604078
Miguel <miguel891906@gmail.com>
2024-03-04 16:16:23 +0100

    commit generar json con datos , falta el value del codigo de familias

--- src/main/java/ExistDB/Data/GenerarJsons/GenerarJsonCentros.java
+++ src/main/java/ExistDB/Data/GenerarJsons/GenerarJsonCentros.java
@@ -1,5 +1,6 @@
 package ExistDB.Data.GenerarJsons;
 
+import ExistDB.Entities.Centro;
 import ExistDB.Entities.Centros;
 
 import com.google.gson.Gson;
@@ -8,6 +9,9 @@ import com.google.gson.GsonBuilder;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GenerarJsonCentros {
     public static void generarJson(Centros centros){
@@ -20,5 +24,21 @@ public class GenerarJsonCentros {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+    }
+    public static String pasarJson(){
+        Centros centros = new Centros();
+        Centro c1 = new Centro();
+        c1.setNombre("c1");
+        Centro c2 = new Centro();
+        c1.setNombre("c2");
+        List<Centro> ListCentro = new ArrayList<>();
+        ListCentro.add(c1);
+        ListCentro.add(c2);
+
+        Gson gson = new Gson();
+        String jsonListaCentros = gson.toJson(ListCentro);
+
+        return jsonListaCentros;
     }
 }
